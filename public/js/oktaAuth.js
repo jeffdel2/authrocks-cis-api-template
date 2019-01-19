e77363b4334ff770d0e05531c45f4605ffe807e3
Glitch (avbank) <none>
2019-01-19 00:20:02 +0000

    🗼💖 Checkpoint ./public/js/oktaAuth.js:1886802/769

--- public/js/oktaAuth.js
+++ public/js/oktaAuth.js
@@ -1,8 +1,7 @@
-console.log("OktaAuth.js");
 var authClient = new OktaAuth(config); // config is defined in views/header.ejs
 
 authClient.session.get().then(function(session) {
-  console.log("OktaAuth.js: got session:");
+  console.log("Got session:");
   console.log(session);
 
   authClient.token.getWithoutPrompt({
@@ -11,21 +10,22 @@ authClient.session.get().then(function(session) {
   }).then(function(tokens) {
     console.log("Got tokens:");
     console.log(tokens);
-    for (var token in tokens) {
-      for (var kind in ['idToken', 'accessToken']) {
+    tokens.forEach(function(token) {
+      ['idToken', 'accessToken'].forEach(function(kind) {
         if (kind in token) {
-          console.log("Received an " + kind + " from Okta");
+          console.log("Received an " + kind + " from Okta:");
           console.log(token);
           authClient.tokenManager.add(kind, token);
         }
-      }
-    }
+      });
+    });
+    // [jpf] FIXME: Is this needed?
     var accessToken = authClient.tokenManager.get('accessToken');
     var base64Url = accessToken.accessToken.split('.')[1];
     var base64 = base64Url.replace('-', '+').replace('_', '/');
     var info = JSON.parse(window.atob(base64));
     console.log(info)
   }).else(function() {
-    console.log("OktaAuth.js: Didn't get tokens");
+    console.log("Didn't get tokens");
   })
 })
