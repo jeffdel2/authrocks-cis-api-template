1ba7ab709c7c49344183235a4356f839bde3829b
Glitch (avbank) <none>
2020-10-16 14:46:51 +0000

    🌽🐝 Checkpoint ./server.js:12838766/3348

--- server.js
+++ server.js
@@ -3,7 +3,73 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    res.send('This is the Fun Auth API');
 });
+
+app.get('/api/public', (req, res) => {
+    let results = {
+      "success": true,
+      "message": "This is the Public API, Anyone can request this"
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(results));
+});
+
+app.get('/api/private', (req, res) => {
+    let results = {
+      "success": true,
+      "message": "This is the private API, Only a valid Okta JWT with a corresponding auth server can see this"
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(results));
+});
+
+app.get('/api/access', (req, res) => {
+    let results = {
+      "success": true,
+      "message": "This is the private API that requires a specifc role to access, Only a valid Okta JWT with the correct claims and a corresponding auth server can see this"
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(results));
+});
+
+
+app.post('/api/access-hook', (req, res) => {
+  
+    let results = {
+          "commands": [
+            {
+              "type": "com.okta.identity.patch",
+              "value": [
+                {
+                  "op": "add",
+                  "path": "/claims/account_number",
+                  "value": "F0" + between(1000, 9999) + "-" + between(1000, 9999)
+                }
+              ]
+            },
+            {
+              "type": "com.okta.access.patch",
+              "value": [
+                {
+                  "op": "add",
+                  "path": "/claims/access",
+                  "value": "GRANTED"
+                }
+              ]
+            }
+          ]
+        };
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(results));
+});
+
+
+function between(min, max) {  
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
+}
+
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
