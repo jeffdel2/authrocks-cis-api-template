0673b11121601e7dc917cb7e9bfe7f420c5d37bf
Glitch (avbank) <none>
2022-03-16 11:05:14 +0000

    🏔🌧 Checkpoint ./config.js:36786028/120

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
   OKTA_HOOK_AUTH:     "1234567890", //Set this as the 'Authentication secret' in your Okta Token Hook
-  EXPECTED_AUDIENCE:  "api://grointel", //This is the Audience in your Okta Authorization Server
-  OKTA_ISSUER:        "https://udp-gro-intel-bc2.oktapreview.com/oauth2/aus15rdm3jlcRu4IB0h8", //This is the Issuer in your Okta Authorization Server
-  OKTA_CLIENT_ID:     "0oa15rdbfgcpFYkU60h8", // This is the cliend Id of the funAuth application in Okta
+  EXPECTED_AUDIENCE:  "api://glitch", //This is the Audience in your Okta Authorization Server
+  OKTA_ISSUER:        "https://delongoie.oktapreview.com/oauth2/aus2939nuuzfZ4jcJ1d7", //This is the Issuer in your Okta Authorization Server
+  OKTA_CLIENT_ID:     "0oa2ys2wk4ekuUaiH1d7", // This is the cliend Id of the funAuth application in Okta
 }
 
 module.exports = config;
