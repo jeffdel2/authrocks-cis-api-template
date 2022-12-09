ea02f27da28aa93e54441eb4a1196cb8c9606034
Glitch (avbank) <none>
2022-12-09 16:53:21 +0000

    🌠🍪 Checkpoint ./config.js:36786028/53

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
   OKTA_HOOK_AUTH:     "1234567890",
-  EXPECTED_AUDIENCE:  "api://glitch",
+  EXPECTED_AUDIENCE:  "api://default",
   OKTA_ISSUER:        "https://delongoie.oktapreview.com/oauth2/aus2939nuuzfZ4jcJ1d7",
-  OKTA_CLIENT_ID:     "0oa2ys2wk4ekuUaiH1d7", // This is the cliend Id of the UI application in Okta
+  OKTA_CLIENT_ID:     "0oa5xwvdp0wFh6eO51d7", // This is the cliend Id of the UI application in Okta
 }
 
 module.exports = config;
